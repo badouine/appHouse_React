9444b23490d06244da328074f50c8b38ba304d80
badouine <nzbadouine@gmail.com>
2022-04-12 05:35:44 +0000

    ui panier

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,11 @@
+import './styles/Cart.css';
+
 const Cart = () => {
     const monsteraPrice = 8
     const ivyPrice = 10
     const flowerPrice = 15
-        return (<div>
+        return (
+        <div className="lmj-cart">
             <h2>Panier</h2>
             <ul>
             <li>Monstera : {monsteraPrice}eu</li>
@@ -10,7 +13,8 @@ const Cart = () => {
             <li>Fleurs: {flowerPrice}eu</li>
             </ul>
             Total: {monsteraPrice + ivyPrice + flowerPrice}eu
-        </div>)
+        </div>
+        )
 }
 
 export default Cart
