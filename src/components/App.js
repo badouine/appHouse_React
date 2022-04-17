b89601639e5098c4e32408106f4dfdb409203384
badouine <nzbadouine@gmail.com>
2022-04-17 05:00:48 +0000

    Evenements

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import Banner from "./Banner";
+import logo from "../assets/logo.png"
+import Footer from './Footer'
 //import Cart from './Cart'; 
 import ShoppingList from './ShoppingList';
+
+
+
 const App = () => {
   return (<div>
-    <Banner/>
+    <Banner>
+        <img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+    </Banner>
   { /* <Cart/> */}  
     <ShoppingList/>
+    <Footer/>
   </div>)
 }
 
