ae903a53ec873531fb0081227d77a106ff047960
badouine <nzbadouine@gmail.com>
2022-04-19 21:46:53 +0000

    charge app

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import Banner from "./Banner";
 import logo from "../assets/logo.png"
 import Footer from './Footer'
-//import Cart from './Cart'; 
+import Cart from './Cart'; 
 import ShoppingList from './ShoppingList';
+import './styles/Layout.css'
 
 
 
 const App = () => {
-  return (<div>
-    <Banner>
-        <img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-    </Banner>
-  { /* <Cart/> */}  
-    <ShoppingList/>
-    <Footer/>
-  </div>)
+  return (
+  
+      <div>
+        <Banner>
+          <img src={logo} alt='La maison jungle' className='lmj-logo' />
+          <h1 className='lmj-title'>La maison jungle</h1>
+        </Banner>
+        <div className='lmj-layout-inner'>
+          <Cart />
+          <ShoppingList />
+        </div>
+        <Footer />
+      </div>
+      )
 }
 
 export default App;
