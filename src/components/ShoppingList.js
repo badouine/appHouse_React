2bd30d6de8bee011a74761e79519f4567c4b8193
badouine <nzbadouine@gmail.com>
2022-04-21 22:54:06 +0000

    props updateCart

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,14 +3,16 @@ import './styles/ShoppingList.css';
 import PlantItem from './PlantItem';
  
 
-function ShoppingList() {
-    const categories = plantList.reduce(
-		(acc, plant) =>
-			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+function ShoppingList({ cart, updateCart}) {
+	// categories nous vient de la partie precedente pour recuperer toutes les categories uniques de plantes 
+
+	const categories = plantList.reduce(
+		(acc, elem) =>
+			acc.includes(elem.category) ? acc : acc.concat(elem.category),
 		[]
 	)
     return (
-		<div>
+		<div className="lmj-shopping-list">
 			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
@@ -18,13 +20,15 @@ function ShoppingList() {
 			</ul>
 			<ul className='lmj-plant-list'>
 				{plantList.map(({ id, cover, name, water, light }) => (
-					<PlantItem
-						id={id}
+					<div key={id}>
+						<PlantItem
 						cover={cover}
 						name={name}
 						water={water}
 						light={light}
 					/>
+					<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+					</div>
 				))}
 			</ul>
 		</div>
